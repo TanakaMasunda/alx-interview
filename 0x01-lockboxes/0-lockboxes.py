cc006240a6c8bed977313713a35f3568525ff326
TanakaMasunda <tankapmasunda@gmail.com>
2023-01-05 23:41:50 +0000

    lockedboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module to check locked boxes can be opened sequencially"""
+
+
+def canUnlockAll(boxes):
+    """method to open the boxes"""
+
+    unlocked = [0]
+    for box_id, box in enumerate(boxes):
+        if not box:
+            continue
+        for key in box:
+            if key < len(boxes) and key not in unlocked and key != box_id:
+                unlocked.append(key)
+                if len(unlocked) == len(boxes):
+                    return True
+                return False
