4ca060b7494b1fa1ff56c8ad5662c6f75a2bab8a
TanakaMasunda <tankapmasunda@gmail.com>
2023-01-05 23:48:59 +0000

    lockedboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -12,6 +12,6 @@ def canUnlockAll(boxes):
         for key in box:
             if key < len(boxes) and key not in unlocked and key != box_id:
                 unlocked.append(key)
-                if len(unlocked) == len(boxes):
-                    return True
-                return False
+    if len(unlocked) == len(boxes):
+        return True
+    return False
