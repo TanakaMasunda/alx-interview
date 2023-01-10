9af0ce149034474318ec83708098cf107aac24c3
TanakaMasunda <tankapmasunda@gmail.com>
2023-01-10 03:11:03 +0000

    minimu operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,14 @@
+# !/usr/bin/python3
+""" minimum operation using only copy all and paste """
+
+
+def minOperations(n):
+    """method calculate the minimum possible number of operations needed
+       to result in exactly n H characters in the file
+    """
+    if n <= 1:
+        return 0
+
+    for i in range(2, n + 1):
+        if n % i == 0:
+            return minOperations(int(n / i)) + i
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,1 @@
+mminimum operations  
