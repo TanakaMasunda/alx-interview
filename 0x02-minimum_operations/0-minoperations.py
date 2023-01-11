58eb494687e5e52609ac41c0fcb2a7fecdcca5bc
TanakaMasunda <tankapmasunda@gmail.com>
2023-01-11 03:13:12 +0000

    readme

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ minimum operation using only copy all and paste """
 
+
 def minOperations(n):
     """method calculate the minimum possible number of operations needed
        to result in exactly n H characters in the file
@@ -10,5 +11,4 @@ def minOperations(n):
 
     for i in range(2, n + 1):
         if n % i == 0:
-            return minOperations(int(n / i)) + i
-        
+            return minOperations(int(n / i)) + i        
