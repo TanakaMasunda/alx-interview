213b2887f1ee0f6c998d94ba04d7cda340420a27
TanakaMasunda <tankapmasunda@gmail.com>
2023-03-03 02:38:42 +0000

    island

--- 0x09_island_perimeter/0-island_perimeter.py
+++ 0x09_island_perimeter/0-island_perimeter.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-  function that returns the perimeter of the island described in grid
-  0 water ,1 land, cell is square(1*1) horizontally or vertically no diagonal
- """
+function that returns the perimeter of the island described in grid
+0 water ,1 land, cell is square(1*1) horizontally or vertically no diagonal
+"""
 
 
 def island_perimeter(grid):
