3c8d30b0ae6b9d2aa1f0ce97ad200320d07607a0
TanakaMasunda <tankapmasunda@gmail.com>
2023-03-12 22:39:50 +0000

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -5,11 +5,12 @@ prime game,2 players drawing out a prime number&its multiples ,last to draw win
 
 
 def isWinner(x, nums):
-    """ You can assume n and x will not be larger than 10000
-        Args:
-            x: is the number of rounds
-            nums: is an array of n
-        Return: name of winner for most rounds& none if no winner is determined
+    """
+    You can assume n and x will not be larger than 10000
+    Args:
+        x: is the number of rounds
+        nums: is an array of n
+    Return: name of winner for most rounds& none if no winner is determined
      """
 
     if x == 1:
